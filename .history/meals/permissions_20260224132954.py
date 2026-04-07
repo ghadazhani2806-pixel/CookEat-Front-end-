f74dedc3f3dce6b77e6ed1ce92280a97308c86a4
ghadazhani2806-pixel <ghadazhani2806@gmail.com>
2026-04-07 15:20:19 +0100

    Ajout de l'application front-end complète

--- .history/meals/permissions_20260224132954.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminUserCustom(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_staff
-
- 
